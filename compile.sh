3a9db4e2f67f5b240e7e97774c20ab46c2cc755e
Charlie Gallagher <charlie@ixisdigital.com>
2022-05-09 21:12:14 -0400

    Add debug symbols to 'as' call and add objcopy
    This may not have been strictly necessary--adding debug symbols--but it
    at least doesn't hurt. I should've included objcopy already in the
    compile bash script. Very soon I'll write a proper Makefile for this,
    but it's just too big a hassle at the moment.
    
    Debugging is working great, though.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-arm-none-eabi-as -o blinky.o as_blinky.s
+arm-none-eabi-as -g -o blinky.o as_blinky.s
 arm-none-eabi-ld -o blinky.elf -T as_blinky_link.ld blinky.o
+arm-none-eabi-objcopy -O binary blinky.elf blinky.bin
